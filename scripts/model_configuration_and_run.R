74432a3fd38f5c55f0033a1a02d3de5c22a609b9
IzzahShah <shahiah@lakeforest.edu>
2026-06-25 14:19:27 -0500

    Generating BETA parameter values and copying to csv

--- scripts/model_configuration_and_run.R
+++ scripts/model_configuration_and_run.R
@@ -11,3 +11,21 @@ ssp245_ini = system.file("input/hector_ssp245.ini", package = "hector")
 # 2.1 Create Hector core
 core <- newcore(ssp245_ini, name = "SSP2-4.5")
 
+# 3.0 Generate BETA param values
+set.seed(25) # standardize random param draws
+
+# 3.1 produce 50 draws of BETA
+param_values <- generate_params(core, draws = 50)
+  
+## sanity check
+head(param_values)
+
+# 3.2 creating BETA parameter df
+beta_values <- param_values %>% 
+  select(BETA)
+
+# 3.3 Add run_numbers to BETA
+beta_values_rn <- beta_values %>% 
+  mutate(run_number = row_number())
+write.csv(beta_values_rn, "data/beta_values_rn.csv", row.names = FALSE)
+
