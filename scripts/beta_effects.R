80c8bb52b7d52e4ae44218fa8e1a309bad88294b
IzzahShah <shahiah@lakeforest.edu>
2026-07-09 14:53:02 -0500

    plotting beta_signal plot and beta_effect plot

--- scripts/beta_effects.R
+++ scripts/beta_effects.R
@@ -17,3 +17,50 @@ beta_effect <-beta_effect_data(lc_metric_results)
 
 # Q2) Compute standardized difference between low and high beta for each variable (Izzah)
 beta_signal <- beta_signal_data(lc_metric_results)
+
+## Plotting Results 
+
+# Q1 -- Plot mean values with CI (5-95%)
+
+beta_effect_plot <- ggplot(
+  data = beta_effect, 
+  aes(
+    x = beta_group,
+    y = mean_value)) + 
+  geom_pointrange(
+    aes(
+    ymin = lower_value,
+    ymax = upper_value)) + 
+  facet_wrap(~variable, scales = "free_y") + 
+  labs(
+    x = "BETA level",
+    y = "Late-century value",
+    title = "Late-century outputs - Difference across BETA levels"
+  ) + 
+  theme_light()
+
+beta_effect_plot
+
+# Q2 
+beta_signal_plot <- 
+  ggplot() + 
+  geom_col(
+    data = beta_signal,
+    aes(
+      x = variable,
+      y = abs_standardized_separation
+    )
+  ) + 
+  labs(
+    x = "Variable",
+    y = "Standardized Difference between low- and high-BETA",
+    title = "Beta-driven differences across variables"
+  ) + 
+  theme_light()
+beta_signal_plot
+  
+
+
+
+
+
