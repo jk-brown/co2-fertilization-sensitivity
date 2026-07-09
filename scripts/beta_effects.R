a891dd2c2cc2cfff85c19d8ee7f2fca907d0b93a
Joe Brown <joseph.brown.k@gmail.com>
2026-07-09 14:03:29 -0500

    Create beta_effects.R

---
+++ scripts/beta_effects.R
@@ -0,0 +1,12 @@
+# BETA Effect Summarization
+
+# Questions
+# 1) How different are model output variables across low, middle, high BETA values. 
+
+# 2) How does the difference between low and high BETA values change through the model 
+# process?
+
+# Q1) Compute the beta effect for all variables using beta_effect_data function (Sofia)
+
+
+# Q2) Compute standardized difference between low and high beta for each variable (Izzah)
